ade4054585435be17f75ba0fbdaba2b64a692117
famesensor <sathit.ju@outlook.com>
2022-06-18 23:28:17 +0700

    [🔧]: update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
         sourceType: 'module',
         tsconfigRootDir: __dirname,
     },
-    plugins: ['@typescript-eslint/eslint-plugin','prettier'],
+    plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
     extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'prettier'],
     root: true,
     env: {
@@ -15,11 +15,11 @@ module.exports = {
     ignorePatterns: ['.eslintrc.js'],
     rules: {
         '@typescript-eslint/interface-name-prefix': 'off',
-        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'warn',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-explicit-any': 'warn',
         '@typescript-eslint/no-unused-vars': ['error'],
         'prettier/prettier': ['error', { usePrettierrc: true }],
         'no-unused-vars': 'off',
     },
-};
+}
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,15 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express(),
     port = 3000
 
-    
+app.use(cors())
 
-app.get('/', (req, res, next) => {
-    console.log(`has a request!`)
+app.get('/', (req, res) => {
     res.status(200).json({ status: 'success' })
 })
 
 app.listen(port, () => {
     console.log(`server running on port -> ${port}`)
 })
-
