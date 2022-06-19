b3cab0f1243b93dc63c51e2463001ee1af06ba27
famesensor <sathit.ju@outlook.com>
2022-06-19 17:27:54 +0700

    [🔨]: add dotenv, update middleware

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 import express from 'express'
 import cors from 'cors'
 
 const app = express(),
-    port = 3000
+    port = process.env.APP_PORT || 3000
 
+// allow cors
 app.use(cors())
+
+// parse application/json
+app.use(express.json())
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.status(200).json({ status: 'success' })
