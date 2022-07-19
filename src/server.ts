dbc546fd993edf3369155a736ab6d5cedefd3da4
famesensor <sathit.ju@outlook.com>
2022-07-19 22:06:42 +0700

    [🎨]: update import code

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-import express from 'express'
 import cors from 'cors'
+import express from 'express'
 import { helloRoute } from './routes'
 
 const app = express(),
